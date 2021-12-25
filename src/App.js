8536e13439d56785d11e750f5fe144d57634acfe
youhane <jason.sebastian154@gmail.com>
2021-12-25 09:10:58 +0700

    gotta learn how to use api

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Footer />
       <h2>Stays in Finland</h2>
       <div className="grid">
         <Card />
       </div>
+      <Footer />
       <GlobalStyle />
     </div>
   );
--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -5,8 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-
 library.add(faStar)
+
+const HOTEL_DATA = [
+    {
+        img: ''
+    }
+]
 
 export default function Card() {
     return (
