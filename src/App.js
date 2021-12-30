779081e129219ce843e4e663d8b1fccd1a061f5d
youhane <jason.sebastian154@gmail.com>
2021-12-30 14:38:51 +0700

    made cards, not styled yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import Card from "./component/Card/Card";
 import Footer from "./component/Footer/Footer";
 import Navbar from "./component/Navbar/Navbar";
@@ -8,21 +6,57 @@ import { GlobalStyle } from "./GlobalStyles";
 const HOTEL_DATA = [
   {
     host: true,
-    room: 'gatau',
-    rating: 7.4,
+    room: 'Entire apartment . 2 beds',
+    rating: 4.40,
+    desc: 'Stylish apartement in center of the city',
+    img: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    host: false,
+    room: 'Private room',
+    rating: 4.25,
+    desc: 'Cozy, peaceful and private room with...',
+    img: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    host: false,
+    room: 'Entire house',
+    rating: 4.96,
+    desc: 'Modern House in a remote area',
+    img: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    host: true,
+    room: 'Entire apartment . 2 beds',
+    rating: 4.85,
+    desc: 'Stylish room in design district',
+    img: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    host: false,
+    room: 'Private room',
+    rating: 4.54,
+    desc: 'Modern apartment close to nature',
+    img: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    host: false,
+    room: 'Entire house',
+    rating: 4.64,
+    desc: 'House in a remote area',
     img: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   },
 ]
 
 function App() {
-  const [imgSrc, setImgSrc] = useState()
-
   return (
     <div className="App">
       <Navbar />
       <h2>Stays in Finland</h2>
       <div className="grid">
-        <Card host room rating img />
+        {HOTEL_DATA.map((hotel) => {
+          return <Card host={hotel.host} room={hotel.room} rating={hotel.rating} desc={hotel.desc} img={hotel.img} />
+        })}
       </div>
       <Footer />
       <GlobalStyle />
--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Wrapper } from './Card.styles'
+import { Wrapper, Host } from './Card.styles'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -7,17 +7,18 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faStar)
 
-export default function Card({ host, room, rating, img }) {
+export default function Card({ host, room, rating, desc, img }) {
     return (
         <div>
-            <img src='' />
+            <img src={img} alt={img} />
             <div>
-                <p>Super Host</p>
-                <p>Entire apartment. 2 beds</p>
+                {host && <Host>Super Host</Host>}
+                <p>{room}</p>
                 <div>
                     <FontAwesomeIcon icon="star" color='red' />
-                    <p>4.40</p>
+                    <p>{rating}</p>
                 </div>
+                <p>{desc}</p>
             </div>
             <Wrapper />
         </div>
